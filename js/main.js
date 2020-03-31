575718a299e44da837263530cb0aaf5edaa8f6d9
davidabutler92 <davidabutler92@yahoo.com>
2020-03-31 16:24:16 -0700

    add for loops with functionality

--- js/main.js
+++ js/main.js
@@ -1,26 +1,65 @@
+//User Interface 
 $(document).ready(function() {
-
   $("#puzzle").submit(function(event) {
     event.preventDefault();
+
     var inputSentence = $("input#sentence").val();
-    
-    var newSentence = [];
+    var userInputArr = inputSentence.split("");
+    // console.log("userInputArr: ", userInputArr)
     var vowel = ["a", "e", "i", "o", "u"];
-    for (i=0; i = inputSentence.length; i +=1)
-      if (inputSentence === vowel) {
-        newSentence = inputSentence.replace(vowel[i], "-");
-        
+    var outPutArr = [];
 
+    for(var i= 0; i < userInputArr.length; i++) {
+      for(var j=0; j < vowel.length; j++) {
+        // console.log("inputSentence[i]: ", inputSentence[i])
+        if(inputSentence[i] === vowel[j]) {
+          outPutArr.push("-");     
+        } else {
+          outPutArr.push(userInputArr[i]);
+        }
       }
+    }
 
-    console.log(newSentence);
-    
 
+    // var fruits = ["Banana", "Orange", "Apple", "Mango"];
+    // var energy = fruits.join("");
+
+
+
+
+
+      // outPutArr.append
+      // var newOutput = outPutArr.join("");
+      // document.write(newOutput);
+      console.log(outPutArr);
+      //  console.log("outPutArr: ", outPutArr);
+               
   });
 });
 
+// var userInput = "12344445";
+// var userInputArr = userInput.split("")
+// var nums = [4,5,6];
+// ouputArr = [];
+
+// for(var i=0; i < userInputArr.length; i++){
+//   for(var j=0; i < nums.length){
+//     if(userInput[i] === nums[j]){
+//       // do something
+//     } else {
+      // do something else
+//     }
+//   }
+// }
 
 
+// i would start by changing this line:
+//  if (inputSentence === vowel) {
+// to this:
+//  if (vowel.includes(inputSentence[i])) {
+// this is a good resource as well: https://www.w3schools.com/jsref/jsref_includes_array.asp
+// basically you need to tell it to look through the array of vowels.
+// if you get stuck still, pop a question into the question channel 
 
 
 
